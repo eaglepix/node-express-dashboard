b8ded1bf472057bb64bdd700ab5e8159d9a9e5f6
Ken Leong <kenleong1@gmail.com>
2021-07-01 22:51:05 +0800

    module4

--- public/javascripts/log-viewer.js
+++ public/javascripts/log-viewer.js
@@ -1,11 +1,18 @@
 const connection = new WebSocket('ws://localhost:3000');
 
 const logWindow = document.querySelector("#log-window");
+const filePath = document.getElementById("#logFilePath");
 
 connection.onopen = () => {
-    connection.send("Hello from the client!");
+    if (!filePath) {
+        console.log("Invalid file path")
+    } else {
+        connection.send(filePath);
+    }
 };
 
 connection.onmessage = (event) => {
-    logWindow.innerHTML = event.data;
+    const logs = event.data.split("\n").join("<hr>");
+    logWindow.innerHTML = logs;
+    // logWindow.innerHTML = event.data;
 }
