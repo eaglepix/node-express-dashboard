9e412d872c583f9c397b64fe350e62c60988dfb5
Ken Leong <kenleong1@gmail.com>
2021-07-02 10:36:24 +0800

    module5

--- services/select-file-service.js
+++ services/select-file-service.js
@@ -1,11 +1,46 @@
+const fs = require('fs');
+const path = require('path');
+const dir = process.cwd();
+
 function getDirectoryContents(files, currentDir, query) {
+    const data = [];
+    files.forEach(file => {
+        if (isDirectory(file)) {
+            data.push({
+                'name': file,
+                'isDirectory': true,
+                'path': path.join(query, file)
+            });
+        } else {
+            data.push({
+                'name': file,
+                'isDirectory': false,
+                'path': path.join(query, file, currentDir)
+            });
+        };
+    });
+    return data;
 }
 
 function isDirectory(currentDir, file) {
+    const fileInfo = fs.statSync(path.join(currentDir, file));
+    return fileInfo;
 }
 
 function readDir(currentDir, res, query) {
+    fs.readdir(currentDir, (err, files) => {
+        const directoryContents = [];
+        if (err) {
+            console.log(err);
+            directoryContents = getDirectoryContents(files, currentDir, query);
+        }
+        res.json(directoryContents);
+    })
 }
 
 exports.get = (req, res) => {
+    let currentDir = dir;
+    const query = req.query.path || "";
+    currentDir = path.join(currentDir, query);
+    readDir(currentDir, res, query);
 };
